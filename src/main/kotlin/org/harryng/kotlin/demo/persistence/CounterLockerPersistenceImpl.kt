78f39a283e64deeb8f76f9155ab410cb78a4ad7c
harryng <harry@videabiz.com>
2021-09-22 09:00:11 +0700

    - Libs beside, reentrance lock is better than sync

--- src/main/kotlin/org/harryng/kotlin/demo/persistence/CounterLockerPersistenceImpl.kt
+++ src/main/kotlin/org/harryng/kotlin/demo/persistence/CounterLockerPersistenceImpl.kt
@@ -6,16 +6,24 @@ open class CounterLockerPersistenceImpl : CounterPersistenceImpl() {
     private val locker = ReentrantLock() //CounterLocker()
 
     override fun increment(id: String, step: Int): Long {
-        locker.lock()
-        val rs = doIncrement(id, step)
-        locker.unlock()
+        var rs: Long
+        try {
+            locker.lock()
+            rs = doIncrement(id, step)
+        } finally {
+            locker.unlock()
+        }
         return rs
     }
 
     override fun currentValue(id: String): Long {
-        locker.lock()
-        val rs = currentCounter(id).value
-        locker.unlock()
+        var rs: Long
+        try {
+            locker.lock()
+            rs = currentCounter(id).value
+        } finally {
+            locker.unlock()
+        }
         return rs
     }
 }
