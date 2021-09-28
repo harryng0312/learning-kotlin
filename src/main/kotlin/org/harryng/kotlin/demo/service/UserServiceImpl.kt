34879a145640377fb7e428c61c0f4a9b21f614a0
harryng <harry@videabiz.com>
2021-09-28 15:15:49 +0700

    - cache - building the gRPC java and kt

--- src/main/kotlin/org/harryng/kotlin/demo/service/UserServiceImpl.kt
+++ src/main/kotlin/org/harryng/kotlin/demo/service/UserServiceImpl.kt
@@ -4,8 +4,9 @@ import org.harryng.kotlin.demo.entity.UserImpl
 import org.harryng.kotlin.demo.persistence.UserPersistence
 import org.harryng.kotlin.demo.util.SessionHolder
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.cache.annotation.Cacheable
 
-class UserServiceImpl :
+open class UserServiceImpl :
     AbstractService<UserImpl, Long>(),
     UserService {
 
@@ -16,6 +17,7 @@ class UserServiceImpl :
 
     }
 
+//    @Cacheable(value= ["user"], key = "#username")
     override fun getByUsername(sessionHolder: SessionHolder, username: String, extras: Map<String, Any>): UserImpl? {
         return persistence.selectByUsername(username)
     }
